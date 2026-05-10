f2260a901f5d7775295c393c2fb8d388294b36fb
darylsmith608-maker <darylsmith608@gmail.com>
2026-05-10 07:04:08 -0400

    Import Supabase client in script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // 1.Initialize Supabase
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uckyzrjhnbcjzyxfrhdg.supabase.co';
 const supabaseKey = 'sb_publishable_tIncfVSxcUDc6ABeg-yULQ_nDyOt1_u';
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
